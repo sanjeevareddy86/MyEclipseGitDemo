ccbd3e2eae120cd69f26365c63a1e228d045a8cf
sanjeeva <dk.sanjeevreddy@gmail.com>
2018-03-05 14:37:05 +0530

    first  commit   Uplodeing  All files

---
+++ Java_Lang_String/src/stringclass/StringDemo.java
@@ -0,0 +1,60 @@
+package stringclass;
+
+public class StringDemo {
+	
+	public  static void main(String  args[])
+	{
+		
+		
+		//String  s="jeeva";
+		
+		//System.out.println(s);
+	//	String s1="jeeva";
+		
+		//System.out.println(s1.equals(s));
+		
+		
+		
+		/*StringBuffer sb=  new StringBuffer("sanjeeva");
+		StringBuffer sb1=new StringBuffer("sanjeeva");
+		System.out.println(sb.equals(sb1));//content  comparisen
+		System.out.println(sb.equals(sb));// referance comparisen 
+
+		//sb.append("reddy");
+		//System.out.println(sb);
+		
+		System.out.println(s.equals("jeeva"));
+		System.out.println(s.equalsIgnoreCase("jeeva"));*/
+		
+		/*
+		 *   //substring
+		         String   name="jeevanSoft";
+				System.out.println(name.substring(3));//vansoft
+				System.out.println(name.substring(0,5));//jeeva 
+		*/
+		
+		/*  //replace(char  old ,char  new)
+		
+		String     s="abab";
+		
+		System.out.println(s.replace('a','b'));
+		
+		*/
+		
+		
+		/*//trim
+		
+		String  s="   San   jeeva ";
+		System.out.println(s.trim());
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
